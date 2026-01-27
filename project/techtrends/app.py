afc41df3546fe19da1e6d029f4568d5f20990f86
m89404 <phid@tdcnet.dk>
2026-01-27 17:08:01 +0100

    feat(Flask): Implement metrics and health endpoints and database wrapper

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,34 +1,81 @@
-import sqlite3
+from http import HTTPStatus
+from contextlib import contextmanager
 
+import sqlite3
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
-# Function to get a database connection.
-# This function connects to database with the name `database.db`
-def get_db_connection():
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    return connection
 
-# Function to get a post using its ID
-def get_post(post_id):
-    connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    connection.close()
-    return post
+class DatabaseWrapper:
+    """
+    Wrapper class for interacting with database
+    """
+    def __init__(self):
+        # Database query count, stored in memory as to report for the running instance itself
+        self.db_connection_count = 0
 
+    @contextmanager
+    def get_db_connection(self) -> sqlite3.Connection:
+        """
+        Function to get a database connection
+        This function connects to database with the name `database.db`
+        :return: Database connection
+        """
+        try:
+            connection = sqlite3.connect('database.db')
+            connection.row_factory = sqlite3.Row
+            yield connection
+        finally:
+            connection.close()
+
+    def execute(self, conn: sqlite3.Connection, sql: str, params=()) -> sqlite3.Cursor:
+        """
+        Execute database query
+        :param conn: Database connection
+        :param sql: SQL to be executed against database
+        :param params: Parameters to substitute in query, if any
+        :return: SQLite cursor
+        """
+        self.db_connection_count += 1
+        return conn.execute(sql, params)
+
+
+def get_post(post_id: int) -> sqlite3.Row:
+    """
+    Function to get a post using its ID
+    :param post_id: ID of the post to get
+    :return: SQLite row of a post
+    """
+    with (db.get_db_connection() as conn):
+        sql_query = 'SELECT * FROM posts WHERE id = ?'
+        post = db.execute(conn, sql_query, (post_id, )).fetchone()
+        return post
+
+
+def get_post_count() -> int:
+    """
+    Get post count
+    :return: Total amount of posts
+    """
+    with db.get_db_connection() as conn:
+        sql_query = 'SELECT COUNT(*) FROM posts'
+        post_count = db.execute(conn, sql_query).fetchone()[0]
+        return post_count
+
+
+db = DatabaseWrapper()
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
+
 # Define the main route of the web application 
 @app.route('/')
 def index():
-    connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
-    return render_template('index.html', posts=posts)
+    with db.get_db_connection() as conn:
+        posts = db.execute(conn, 'SELECT * FROM posts').fetchall()
+        return render_template('index.html', posts=posts)
+
 
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
@@ -36,16 +83,18 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
-      return render_template('404.html'), 404
+      return render_template('404.html'), HTTPStatus.NOT_FOUND
     else:
       return render_template('post.html', post=post)
+
 
 # Define the About Us page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-# Define the post creation functionality 
+
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -55,16 +104,46 @@ def create():
         if not title:
             flash('Title is required!')
         else:
-            connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            connection.commit()
-            connection.close()
-
-            return redirect(url_for('index'))
+            with db.get_db_connection() as conn:
+                db.execute(conn, 'INSERT INTO posts (title, content) VALUES (?, ?)',
+                             (title, content))
+                db.db_connection_count += 1
+                conn.commit()
+                return redirect(url_for('index'))
 
     return render_template('create.html')
 
+@app.route('/healthz')
+def healthz():
+    """
+    Check health of the application
+    :return: Health of the application
+    """
+    response = app.response_class(
+        response=json.dumps({'result': 'OK - healthy'}),
+        status=HTTPStatus.OK,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route('/metrics')
+def metrics_endpoint():
+    """
+    Get metrics of application
+    :return: Metrics of the application
+    """
+    post_count = get_post_count()
+    response = app.response_class(
+        response=json.dumps({
+            'db_connection_count': db.db_connection_count,
+            'post_count': post_count
+        }),
+        status=HTTPStatus.OK,
+        mimetype='application/json'
+    )
+    return response
+
+
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+   app.run(host='0.0.0.0', port=3111)
